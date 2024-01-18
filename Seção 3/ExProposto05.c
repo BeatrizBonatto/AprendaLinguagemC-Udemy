cd100e6f6651136f772e5b98ce0e60e1057f23b9
Beatriz <beatriz.bonatto@fatec.sp.gov.br>
2024-01-17 22:33:13 -0300

    :heavy_plus_sign: build Exercício 5 e Exemplo for

---
+++ Seção 3/ExProposto05.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*5) Elabore um fluxograma que receba um valor inteiro digitado 
+pelo usuário e verifique se este se encontra na faixa de zero a 10. 
+Mostre mensagem informando se o valor está dentro da faixa permitida 
+ou se está fora da faixa.*/
+
+int main(void)
+{
+    
+}
